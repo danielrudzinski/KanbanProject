190a95d30376ac12f6022ef4b76ca83bd8317d3c
danielrudzinski <danielrudzinski2k3@gmail.com>
2025-03-20 23:17:30 +0100

    lob to title

--- src/main/java/pl/myproject/kanbanproject2/model/SubTask.java
+++ src/main/java/pl/myproject/kanbanproject2/model/SubTask.java
@@ -16,6 +16,7 @@ public class SubTask {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Lob
     private String title;
     @Lob
     private String description;
