be0f53b8e412a18542ed23092abdc55183361770
danielrudzinski <danielrudzinski2k3@gmail.com>
2025-03-22 18:47:49 +0100

    dotenv added

---
+++ src/main/java/pl/myproject/kanbanproject2/config/KanbanConfig.java
@@ -0,0 +1,28 @@
+package pl.myproject.kanbanproject2.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/*
+@Configuration
+public class KanbanConfig {
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+    @Bean
+    public String dataSourceUrl() {
+        return dotenv.get("SPRING_DATASOURCE_URL");
+    }
+
+    @Bean
+    public String dataSourceUsername() {
+        return dotenv.get("SPRING_DATASOURCE_USERNAME");
+    }
+
+    @Bean
+    public String dataSourcePassword() {
+        return dotenv.get("SPRING_DATASOURCE_PASSWORD");
+    }
+}*/
