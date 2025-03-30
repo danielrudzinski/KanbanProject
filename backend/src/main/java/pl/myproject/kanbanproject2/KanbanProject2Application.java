7f2eb7fde4a6b576e0a6a898f09b65cd7772cf5a
danielrudzinski <danielrudzinski2k3@gmail.com>
2025-03-31 00:33:51 +0200

    if statement in main abt production on azure

--- backend/src/main/java/pl/myproject/kanbanproject2/KanbanProject2Application.java
+++ backend/src/main/java/pl/myproject/kanbanproject2/KanbanProject2Application.java
@@ -1,16 +1,29 @@
-package pl.myproject.kanbanproject2;
 
+package pl.myproject.kanbanproject2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class KanbanProject2Application {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.out.println("SPRING_DATASOURCE_URL: " + dotenv.get("SPRING_DATASOURCE_URL"));
-        System.out.println("SPRING_DATASOURCE_USERNAME: " + dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.out.println("SPRING_DATASOURCE_PASSWORD: " + dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        String url = System.getenv("SPRING_DATASOURCE_URL");
+        String username = System.getenv("SPRING_DATASOURCE_USERNAME");
+        String password = System.getenv("SPRING_DATASOURCE_PASSWORD");
+
+        if (url == null || username == null || password == null) {
+            System.out.println("Environment variables not found. Loading from .env...");
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+            url = dotenv.get("SPRING_DATASOURCE_URL");
+            username = dotenv.get("SPRING_DATASOURCE_USERNAME");
+            password = dotenv.get("SPRING_DATASOURCE_PASSWORD");
+        }
+
+        System.out.println("SPRING_DATASOURCE_URL: " + url);
+        System.out.println("SPRING_DATASOURCE_USERNAME: " + username);
+        System.out.println("SPRING_DATASOURCE_PASSWORD: " + password);
 
         SpringApplication.run(KanbanProject2Application.class, args);
     }
