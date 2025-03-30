041a00c99aa10ee2c2c9dc082723cb4d0e8dc898
danielrudzinski <danielrudzinski2k3@gmail.com>
2025-03-30 19:46:48 +0200

    env added
    added env

--- backend/src/main/java/pl/myproject/kanbanproject2/KanbanProject2Application.java
+++ backend/src/main/java/pl/myproject/kanbanproject2/KanbanProject2Application.java
@@ -3,13 +3,15 @@ package pl.myproject.kanbanproject2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.cdimascio.dotenv.Dotenv;
-
-
 @SpringBootApplication
 public class KanbanProject2Application {
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.out.println("SPRING_DATASOURCE_URL: " + dotenv.get("SPRING_DATASOURCE_URL"));
+        System.out.println("SPRING_DATASOURCE_USERNAME: " + dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.out.println("SPRING_DATASOURCE_PASSWORD: " + dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
         SpringApplication.run(KanbanProject2Application.class, args);
     }
 }
-
-
