dce00b4e0bf1c39ad11b1aa997bd72b4bf7e7d9d
danielrudzinski <danielrudzinski2k3@gmail.com>
2025-03-30 16:31:13 +0200

    feat

--- backend/src/main/java/pl/myproject/kanbanproject2/KanbanProject2Application.java
+++ backend/src/main/java/pl/myproject/kanbanproject2/KanbanProject2Application.java
@@ -2,12 +2,20 @@ package pl.myproject.kanbanproject2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
-@SpringBootApplication
 public class KanbanProject2Application {
-
     public static void main(String[] args) {
+        // Załaduj zmienne środowiskowe z pliku .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Opcjonalnie, ustaw zmienne środowiskowe, jeśli nie są automatycznie rozpoznawane
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
+        // Uruchom aplikację Spring Boot
         SpringApplication.run(KanbanProject2Application.class, args);
     }
-
 }
+
