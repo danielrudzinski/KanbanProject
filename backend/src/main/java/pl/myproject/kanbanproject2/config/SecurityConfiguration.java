22aaef7b1a2a82ed8b2f781f789497944ef179c1
dominiksmolinski3 <dominik.smolinski19@gmail.com>
2025-06-13 00:59:29 +0200

    security update for deploy

--- backend/src/main/java/pl/myproject/kanbanproject2/config/SecurityConfiguration.java
+++ backend/src/main/java/pl/myproject/kanbanproject2/config/SecurityConfiguration.java
@@ -34,6 +34,8 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/health/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/ws/**").permitAll() 
                         .requestMatchers("/chat").permitAll()
