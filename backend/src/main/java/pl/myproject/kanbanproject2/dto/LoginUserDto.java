b2ba316568c50f3ce8011bb7ec39a767c9f28f08
cynio <dominik.smolinski19@gmail.com>
2025-08-15 13:17:53 +0200

    fix(backend): add @NotBlank annotation

--- backend/src/main/java/pl/myproject/kanbanproject2/dto/LoginUserDto.java
+++ backend/src/main/java/pl/myproject/kanbanproject2/dto/LoginUserDto.java
@@ -2,11 +2,13 @@ package pl.myproject.kanbanproject2.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class LoginUserDto {
     private String email;
     private String password;
+    @NotBlank
     private String captchaToken;
 }
--- backend/src/main/java/pl/myproject/kanbanproject2/dto/RegisterUserDto.java
+++ backend/src/main/java/pl/myproject/kanbanproject2/dto/RegisterUserDto.java
@@ -1,6 +1,7 @@
 package pl.myproject.kanbanproject2.dto;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -8,5 +9,6 @@ public class RegisterUserDto {
     private String email;
     private String password;
     private String username;
+    @NotBlank
     private String captchaToken;
 }
